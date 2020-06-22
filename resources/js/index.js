bd36d1523a4683638ecb58bffb9e59118386dab8
mj <misd.markjulius.riitmc@gmail.com>
2020-06-22 12:23:07 +0800

    first commit

---
+++ resources/js/index.js
@@ -0,0 +1,10 @@
+require('./bootstrap');
+
+import Vue from 'vue'
+import vuetify from './plugins/vuetify' // path to vuetify export
+import App from './App.vue'
+
+new Vue({
+  vuetify,
+  render: h => h(App),
+}).$mount('#app');
